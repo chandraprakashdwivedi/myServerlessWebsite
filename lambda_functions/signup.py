b2b04980678edfb4c835c7f57ed751bc0b892d33
CP <chandra.prakash363@gmail.com>
2019-04-26 11:29:03 +0530

    Update signup.py

--- lambda_functions/signup.py
+++ lambda_functions/signup.py
@@ -4,6 +4,7 @@ import boto3
 dynamodb = boto3.resource('dynamodb')
 
 def lambda_handler(event, context):
+    print("event", event)
     table = dynamodb.Table('sls-website-dev-table1')
     if 'body' in event and event['body'] is not None:
         body = json.loads(event['body'])
@@ -11,15 +12,12 @@ def lambda_handler(event, context):
         "user_name": body['user_name'],
         "password": body['password']
         }
-        table.put(item)
+    table.put_item(Item=item)
     
     resp = {
         "statusCode": 200,
-        "headers": {
-            "Access-Control-Allow-Origin": "*",
-            "body": "Item Inserted successfully"
-        },
-       
+        "body":json.dumps('Item Inserted successfully')
+
     }
     
     return resp
